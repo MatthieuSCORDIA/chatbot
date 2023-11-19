9b2d7b2e59e0e5181445ae6e4256be59835ba410
MatthieuSCORDIA <matth.scordia@efrei.net>
2023-11-19 18:02:01 +0100

    include the speeches

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from Partie_de_matthieu import * # ajout du travail de Matthieu
 directory = "speeches-20231119"  # séléction du dossier
 files_names = list_of_files(directory, "txt") # récupération des noms de fichiers
-print_list(files_names) # afficher les listes
+print_list(files_names) # afficher la liste des fichiers
