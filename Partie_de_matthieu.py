1412dc8934d5def61523c2e8adb0464dade98406
MatthieuSCORDIA <matth.scordia@efrei.net>
2023-11-21 09:08:32 +0100

    print names of presidents(with dubble)

--- Partie_de_matthieu.py
+++ Partie_de_matthieu.py
@@ -1,5 +1,6 @@
 import os
 
+
 def print_list(lists_printer):
     for list_printer in lists_printer:
         print(list_printer)
@@ -15,10 +16,19 @@ def list_of_files(directory, extension):
 
 def print_president():
     from main import directory
-    files_names_president = list_of_files(directory,"txt")
+    files_names_president = list_of_files(directory, "txt")
     for boucle1_president in range(len(files_names_president)):
-        files_names_president[boucle1_president]= files_names_president[boucle1_president].replace("Nomination_", "")
+        files_names_president[boucle1_president] = files_names_president[boucle1_president].replace("Nomination_", "")
         files_names_president[boucle1_president] = files_names_president[boucle1_president].replace(".txt", "")
         files_names_president[boucle1_president] = files_names_president[boucle1_president].replace("1", "")
         files_names_president[boucle1_president] = files_names_president[boucle1_president].replace("2", "")
+    boucle1_president = 0
+    while boucle1_president < len(files_names_president):
+        boucle2_president = 0
+        while boucle2_president < len(files_names_president):
+            if boucle1_president != boucle2_president and files_names_president[boucle1_president] == \
+                    files_names_president[boucle2_president]:
+                files_names_president.pop(boucle2_president)
+            boucle2_president += 1
+        boucle1_president += 1
     print_list(files_names_president)
