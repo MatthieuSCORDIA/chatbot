4287fcdb1869a0b7793792742620796814b7ef82
MatthieuSCORDIA <matth.scordia@efrei.net>
2023-11-21 08:54:31 +0100

    print names of presidents

--- Partie_de_matthieu.py
+++ Partie_de_matthieu.py
@@ -1,6 +1,5 @@
 import os
 
-
 def print_list(lists_printer):
     for list_printer in lists_printer:
         print(list_printer)
@@ -12,3 +11,14 @@ def list_of_files(directory, extension):
         if filename.endswith(extension):
             files_names.append(filename)
     return files_names
+
+
+def print_president():
+    from main import directory
+    files_names_president = list_of_files(directory,"txt")
+    for boucle1_president in range(len(files_names_president)):
+        files_names_president[boucle1_president]= files_names_president[boucle1_president].replace("Nomination_", "")
+        files_names_president[boucle1_president] = files_names_president[boucle1_president].replace(".txt", "")
+        files_names_president[boucle1_president] = files_names_president[boucle1_president].replace("1", "")
+        files_names_president[boucle1_president] = files_names_president[boucle1_president].replace("2", "")
+    print_list(files_names_president)
