7d2a24edc632eadecde0c9263507f6f6370f039d
MatthieuSCORDIA <matth.scordia@efrei.net>
2023-11-22 09:52:15 +0100

    create the part of loic

--- main.py
+++ main.py
@@ -1,3 +1,2 @@
-from Partie_de_matthieu import *  # ajout du travail de Matthieu
-files_names = list_of_files(directory, "txt")  # récupération des noms de fichiers
+from Partie_de_loic import *  # ajout du travail de Matthieu et Loic
 print_president()  # afficher la liste des présidents
