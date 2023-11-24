dac3fc1400b66e06047ce6e2d90ee2707d58b7ad
AnglardLoic <loic.anglard@efrei.net>
2023-11-24 11:21:40 +0100

    V1

--- Partie_de_loic.py
+++ Partie_de_loic.py
@@ -1,3 +1,12 @@
-from Partie_de_matthieu import *# ajout du travail de Matthieu
-directory = "speeches-20231119"  # séléction du dossier
-files_names = list_of_files(directory, "txt")  # récupération des noms de fichiers
+from Partie_de_matthieu import *  # ajout du travail de Matthieu
+
+
+def cleaner():
+    directory = "speeches-20231119"  # séléction du dossier
+    files_names = list_of_files(directory, "txt")  # récupération des noms de fichiers
+
+    for fichier in files_names:
+        with open(directory+"/"+fichier, "r") as fichier_ouvert:
+            textenonclean = fichier_ouvert.readline()
+            if fichier
+            print(textenonclean)
