8dcc4156dc1a0f8b54d7103e819ea460d8f9f646
MatthieuSCORDIA <matth.scordia@efrei.net>
2023-12-01 13:51:32 +0100

    Changed main

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from Partie_de_loic import *  # ajout du travail de Matthieu et Loic
 print_president()  # afficher la liste des présidents
 print_list(TF_IDF())
+dico_TF = TF("cleaned")
+dico_IDF = IDF("cleaned")
+dico_TF_IDF = TF_IDF("cleaned")
